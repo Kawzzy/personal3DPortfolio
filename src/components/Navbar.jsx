aeb1753d15b200ef6fdcd25b35184fae8ad2abe9
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-03-26 18:07:49 -0300

    Correcting a navbar error.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 "bg-primary"
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary
       `}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
@@ -23,7 +23,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-20 h-20 object-contain" />
+          <img src={logo} alt="logo" className="w-16 h-16 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
             Kawzzy &nbsp;
             <span className="sm:block hidden"> | Augusto Kalahary</span>
